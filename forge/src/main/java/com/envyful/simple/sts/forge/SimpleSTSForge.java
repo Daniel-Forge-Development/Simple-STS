ebfb43f2dffeb5c330deae1686459beee0c8eb05
Daniel Norris <dnorris443@gmail.com>
2021-11-10 15:31:55 +0000

    chore(gradle): bump version

--- forge/src/main/java/com/envyful/simple/sts/forge/SimpleSTSForge.java
+++ forge/src/main/java/com/envyful/simple/sts/forge/SimpleSTSForge.java
@@ -24,7 +24,7 @@ import java.io.IOException;
 )
 public class SimpleSTSForge {
 
-    public static final String VERSION = "1.7.0";
+    public static final String VERSION = "1.0.0";
 
     private static SimpleSTSForge instance;
 
