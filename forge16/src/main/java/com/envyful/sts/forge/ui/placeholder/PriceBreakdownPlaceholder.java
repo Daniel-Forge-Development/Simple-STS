d94c8787d9271518053df998987c5d9c08f2279f
Daniel Norris <dnorris443@gmail.com>
2023-01-10 02:50:32 +0000

    feat(ui): add PriceBreakdownPlaceholder

---
+++ forge16/src/main/java/com/envyful/sts/forge/ui/placeholder/PriceBreakdownPlaceholder.java
@@ -0,0 +1,45 @@
+package com.envyful.sts.forge.ui.placeholder;
+
+import com.envyful.api.text.parse.MultiPlaceholder;
+import com.envyful.sts.forge.config.DisplayablePokeSpecPricing;
+import com.google.common.collect.Lists;
+import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
+import net.minecraft.entity.player.ServerPlayerEntity;
+
+import java.util.Collections;
+import java.util.List;
+
+public class PriceBreakdownPlaceholder implements MultiPlaceholder {
+
+    private final ServerPlayerEntity player;
+    private final Pokemon pokemon;
+    private final List<DisplayablePokeSpecPricing> modifiers;
+
+    public PriceBreakdownPlaceholder(ServerPlayerEntity player, Pokemon pokemon, List<DisplayablePokeSpecPricing> modifiers) {
+        this.player = player;
+        this.pokemon = pokemon;
+        this.modifiers = modifiers;
+    }
+
+
+    @Override
+    public List<String> replace(String s) {
+        if (!s.contains("%price_breakdown%")) {
+            return Collections.singletonList(s);
+        }
+
+        List<String> entries = Lists.newArrayList();
+
+        for (DisplayablePokeSpecPricing minPriceModifier : this.modifiers) {
+            if (!minPriceModifier.hasPermission(this.player)) {
+                continue;
+            }
+
+            if (minPriceModifier.getSpec().matches(pokemon)) {
+                entries.add(minPriceModifier.getDisplay());
+            }
+        }
+
+        return entries;
+    }
+}
