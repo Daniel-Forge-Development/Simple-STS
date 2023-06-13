46d2f802356e8574f9dd7700008b7d9e7b67eb82
Daniel Norris <dnorris443@gmail.com>
2023-06-13 11:32:58 +0100

    chore(gradle): bump version to 2.1.7

--- forge/src/main/java/com/envyful/sts/forge/EnvySTSForge.java
+++ forge/src/main/java/com/envyful/sts/forge/EnvySTSForge.java
@@ -24,7 +24,7 @@ import java.io.IOException;
 )
 public class EnvySTSForge {
 
-    public static final String VERSION = "2.1.6";
+    public static final String VERSION = "2.1.7";
 
     private static EnvySTSForge instance;
 
